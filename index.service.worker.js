efb90c5d7a5bfbb850bb0205fa385a1d48a481fa
Mable <99951964+mable-yeah@users.noreply.github.com>
2026-04-11 11:18:05 -0500

    help button
    clarify how to open an image

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1775921265|578862469';
+const CACHE_VERSION = '1775925199|132224366';
 /** @type {string} */
 const CACHE_PREFIX = 'effectinator-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
